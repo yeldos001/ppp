63bf1d8ef6ac11b0f1b1199577808b17d5f866b1
yeldos001 <kadyrzaneldos39@gmail.com>
2025-03-01 16:43:53 +0500

    Delete Lab5/regex.md directory

--- Lab5/regex.md/6.py
+++
@@ -1,7 +0,0 @@
-#Напишите программу на Python для замены всех вхождений пробела, запятой или точки на двоеточие.
-import re
-
-text = "Hello, world. How are you?"
-
-result = re.sub(r"[ ,.]", ":", text)
-print(result)
--- Lab5/regex.md/8.py
+++
@@ -1,7 +0,0 @@
-#Напишите программу на Python для разделения строки на прописные буквы.
-import re
-
-text = "HelloWorldPython"
-
-result = re.split(r"(?=[A-Z])", text)
-print(result)
